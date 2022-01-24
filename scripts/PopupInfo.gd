920e71d7cb8284137afab9bc7b740a2609203a26
ilhamsrajab <iamidat@outlook.com>
2022-01-24 15:41:40 +0700

    fix popup kalah

--- scripts/PopupInfo.gd
+++ scripts/PopupInfo.gd
@@ -6,7 +6,7 @@ func _ready():
 	pass
 	
 func muncul():
-	tween.interpolate_property(self, "rect_position:y", -600, 0, 1, Tween.TRANS_ELASTIC, Tween.EASE_OUT)
+	tween.interpolate_property(self, "rect_position:y", -1080, 0, 1, Tween.TRANS_ELASTIC, Tween.EASE_OUT)
 	tween.start()
 	
 	yield(tween, "tween_all_completed")
@@ -14,7 +14,7 @@ func muncul():
 
 
 func _on_btn_next_pressed():
-	tween.interpolate_property(self, "rect_position:y", 0, -600, 1, Tween.TRANS_ELASTIC, Tween.EASE_IN)
+	tween.interpolate_property(self, "rect_position:y", 0, -1080, 1, Tween.TRANS_ELASTIC, Tween.EASE_IN)
 	tween.start()
 	
 	yield(tween, "tween_all_completed")
--- scripts/PopupTBC.gd
+++ scripts/PopupTBC.gd
@@ -8,7 +8,7 @@ func _ready():
 	pass
 	
 func muncul():
-	tween.interpolate_property(self, "rect_position:y", -600, 0, 1, Tween.TRANS_ELASTIC, Tween.EASE_OUT)
+	tween.interpolate_property(self, "rect_position:y", -1080, 0, 1, Tween.TRANS_ELASTIC, Tween.EASE_OUT)
 	tween.start()
 	
 	yield(tween, "tween_all_completed")
